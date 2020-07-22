4f7a8a15823cb7ab388895cdbad8290bec855a9d
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-07-22 15:50:27 +0100

    First verdsion - makes list of sorted CSS styles
    CMK's challenge

---
+++ audio1.py
@@ -0,0 +1,40 @@
+from requests_html import HTMLSession
+import json
+from operator import itemgetter
+#
+url='http://audioeden.com/useddemo-gear/4525583102'
+session = HTMLSession()
+#
+ls = []
+# Make a List of dictionaries (CSS Name, left value, top value)
+def css_dict(style):
+    dct = {}
+    try:
+        # css name
+        dct['name'] = style.split("{")[0].replace("\n","").strip()
+        # left px
+        left = style.split("left")[1].replace("\n","").replace(":","").strip().split(";")[0].replace("px","")
+        dct['left'] = int(left)
+        # top px
+        top = style.split("left")[1].replace("\n","").replace(":","").strip().split(";")[1].split("top")[1].strip().replace("px","")
+        dct['top'] = int(top)
+        ls.append(dct)
+    except:
+        pass
+#
+r = session.get(url)
+style = r.html.xpath("//style/text()")
+#
+# put CSS into list
+style = "".join(style)
+style = style.split("}")
+
+for i in range (0,333):
+    #print(style[i])
+    css_dict(style[i])
+
+#print(ls) - sorted by "left px", then "top px"
+ls.sort(key=itemgetter('left'))
+print(ls)
+
+# next get matching div class name and get its text #
